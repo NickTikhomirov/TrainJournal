4e89e5a91bac19fd08075f56431d2b50b907eaaa
OzoNeTT <45043082+OzoNeTT@users.noreply.github.com>
2020-04-10 01:01:38 +0300

    Redo datagenerator

--- MyNewProject/MyNewProject/Date.h
+++ MyNewProject/MyNewProject/Date.h
@@ -1,113 +1,74 @@
 #pragma once
-#include <iostream>
 #include <string>
 #include <vector>
 #include <ctime>
 
-// СУКА НЕ СТИРАЙ ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ, А ТО Я УЖЕ САМ НЕ ЕБУ КАК ЭТО РАБОТАЕТ
 int startDate;
 int startMon;
+std::string weekday[] = { "Воскресенье", "Понедельник", "Вторник", "Среда","Четверг", "Пятница", "Суббота"};
 
-// ЭТО НА 2020 ГОД КОЛ-ВО ДНЕЙ В МЕСЯЦАХ
-int dayInMonths[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
-// ВОТ ТУТ ЦИФЕРКИ - ЭТО ДЕНЬ НЕДЕЛИ. ПН - 1, ВТ - 2, ..., ВС-0
-int startDayInMonth[12] = {3,6,0,3,5,1,3,6,2,4,0,2};
+std::vector<time_t> dates;
 
-// ВОТ СЮДА ОНИ ВСЕ ЛЕТЯТ
-std::vector<tm> dates;
+//Рандом дат, НЕ ТРОГАТЬ 
+void RandomDateGenerator(int N, std::time_t curtime) {
 
-void RandomDateGenerator(int N, int tm_sec, int tm_yday, int tm_year, int tm_isdst) {
+    tm * timeinfo = std::localtime(&curtime);
+    timeinfo->tm_min = rand() % 59;
+    timeinfo->tm_hour = 10 + rand() % 8;
+    timeinfo->tm_mday = startDate;
+    timeinfo->tm_mon = startMon;
+    dates.push_back(std::mktime(timeinfo));
 
-    int tm_min;
-    int tm_hour;
-    int iday;
-    int tm_mon = startMon;
-    int tm_mday = startDate;
+    bool zashel = false;
+    tm * somedate = std::localtime(&curtime);
+    for(size_t i = 0; i < N-1; i++) {
 
-    //----генерим время (часы и минуты)
-    tm_min = rand() % 59;
-    tm_hour = 10 + rand() % 8;
-    //---------------------------------
-
-    //-------тут определяем день недели, НЕ РЕДАЧИТЬ, А ТО ВСЕ ПОЛЕТИТ
-    iday = startDate;
-    while (iday > 6 - startDayInMonth[startMon]) {
-        iday -= 7;
-    }
-    iday -= 1;
-    int someday = startDayInMonth[startMon] + iday;
-    if (someday < 0) {
-        someday = 7 + someday;
-    }
-    //-----Заносим первый день
-    const struct tm startDateInTm = {tm_sec, tm_min, tm_hour, startDate, startMon, tm_year, someday, tm_yday, tm_isdst};
-    dates.push_back(startDateInTm);
-    //-------------------------------------------
-
-    //------Генерим новый день-----------------
-    for(int i = 0; i < N - 1; i++) {
-        //----------находим новый день в месяце
-        tm_mday += 2 + rand() % 3;
-        //--------проверяем, а не след ли месяц
-        if(tm_mday > dayInMonths[tm_mon]){
-            tm_mday = tm_mday - dayInMonths[tm_mon];
-            tm_mon++;
+        somedate->tm_min = rand() % 59;
+        somedate->tm_hour = 10 + rand() % 8;
+        if(!zashel) {
+            somedate->tm_mday = startDate+ 2 + rand() % 3;
+            zashel = true;
         }
-        //-------ну тут время делаем
-        tm_min = rand() % 59;
-        tm_hour = 10 + rand() % 8;
-
-        //--------Тут определяем день недели, НЕ РЕДАЧИТЬ, А ТО ВСЕ ПОЛЕТИТ
-        iday = tm_mday;
-        while (iday > 6 - startDayInMonth[tm_mon]) {
-            iday -= 7;
-        }
-        iday -= 1;
-        int tm_wday = startDayInMonth[tm_mon] + iday;
-        if (tm_wday < 0) {
-            tm_wday = 7 + tm_wday;
-        }
-        //---Заносим дни
-        const struct tm generatedDate = {tm_sec, tm_min, tm_hour, tm_mday, tm_mon, tm_year, tm_wday, tm_yday, tm_isdst};
-        dates.push_back(generatedDate);
+        somedate->tm_mday = somedate->tm_mday + 2 + rand() % 3;
+        std::mktime(somedate);
+        dates.push_back(std::mktime(somedate));
     }
-    //------------------------------------
-
 }
 
-//-------А это для вывода нахуй!--КУДА?????? ДА КУДА ХОЧЕШЬ НАХУЙ НАХУЙ!
-std::string DateToString(std::vector<tm> dates){
+//функция парсинга, МОЖНО ВИДОИЗМЕНЯТЬ
+std::vector<std::string> parseDateToString(std::vector<time_t > datesToParse){
+    std::vector<std::string> parsedData;
     std::string str = "";
-    return str;
+    for (auto it : datesToParse){
+        str =   std::to_string(std::localtime(&it)->tm_mday) + "." +
+                std::to_string(std::localtime(&it)->tm_mon) + "." +
+                std::to_string(std::localtime(&it)->tm_year + 1900)  + "\t" +
+                weekday[std::localtime(&it)->tm_wday] + "\t\t" +
+                std::to_string(std::localtime(&it)->tm_hour) + ":" +
+                std::to_string(std::localtime(&it)->tm_min) + "\n";
+        parsedData.push_back(str);
+    }
+    return parsedData;
 }
 
 int main(){
     srand(time(0));
-    /*
-     * Изначально есть период отсчета, определяем его в tm
-     * после генерим два дня в неделе
-     * узнаем для них число и месяц
-     * генерим время
-     * готово
-     */
 
-    //base:
     std::time_t curtime = std::time(nullptr);
-    int tm_sec = 0;
-    int tm_yday = std::localtime(&curtime)->tm_yday;
-    int tm_year= std::localtime(&curtime)->tm_year;
-    int tm_isdst = std::localtime(&curtime)->tm_isdst;
-    //---
 
+    //ВОТ ТУТ СКОЛЬКО НАМ НУЖНО ЗАНЯТИЙ
+    int N = 30;
+    
+    //ЧИСЛО МЕСЯЦА НАЧАЛА 
+    startDate = 30;
+    //МЕСЯЦ НАЧАЛА
+    startMon = 3;
 
-    int N = 30;   // ВОТ ЭТО СКОЛЬКО ДНЕЙ ХОТИМ
-    startDate = 23; // ВОТ ЭТО ДАТА КОГДА МЫ НАЧИНАЕМ ЗАНИМАТЬСЯ (ОНА ВХОДИТ В N)
-    startMon = 3; // ВОТ ЭТО НАЧАЛЬНЫЙ МЕСЯЦ, КОГДА МЫ ФИГАРИТЬ НАЧИНАЕМ
+    //НУ СОБСНА РАБОТА
+    RandomDateGenerator(N, curtime);
 
-    RandomDateGenerator(N, tm_sec, tm_yday, tm_year, tm_isdst);
-    //-----ВОТ ТАК В КОНСОЛЬ-------
-    for(auto it : dates)
-        std::cout << std::asctime(&it) << std::endl;
-    //---НЕ ЗАБУДЬ ССЫЛКУ ЕБУЧУЮ 
+    //ТУТ ПОЛУЧАЕМ СТРОКУ ЕЕ ПИХАЕМ КУДА УГОДНО
+    std::vector<std::string> PD = parseDateToString(dates);
+
     return 0;
 }
