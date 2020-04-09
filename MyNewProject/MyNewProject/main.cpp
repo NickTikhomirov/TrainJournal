d7758956bfd54f90a8b5b19fe7a87dd0ba869ddd
NickTikhomirov <nicktikhomirov02@gmail.com>
2020-04-09 16:18:40 +0300

    one more initial commit

---
+++ MyNewProject/MyNewProject/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::unordered_set;
+using std::unordered_map;
+using std::function;
+
+
+int main() {
+
+
+
+}
